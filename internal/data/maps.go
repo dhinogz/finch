f2d41e3c3f54dd74ab7a5a64cfb503cbe5f70e18
David Sanchez <A01720476@itesm.mx>
2023-09-23 19:23:45 -0600

    polyline decode

--- internal/data/maps.go
+++ internal/data/maps.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/dhinogz/finch/pkg/flexpolyline"
 	"googlemaps.github.io/maps"
 )
 
@@ -44,14 +45,14 @@ func (mm *MapModel) GetDefaultRoute() (*Route, error) {
 
 	route := &Route{
 		Start: Place{
-			Name: "Berlin1",
-			Lat:  52.5222969,
-			Lng:  13.3532959,
+			Name: "Teconologico de Monterrey",
+			Lat:  25.650711,
+			Lng:  -100.289449,
 		},
 		End: Place{
-			Name: "Berlin2",
-			Lat:  52.508309,
-			Lng:  13.355633,
+			Name: "Casita de la abuela",
+			Lat:  25.636026,
+			Lng:  -100.315968,
 		},
 	}
 
@@ -119,10 +120,10 @@ func (mm *MapModel) CalcRoute() {
 	// 	"&waypoint1=geo!52.5,13.45" +
 	// 	"&mode=fastest;car;traffic:disabled"
 
-	origin := "52.522297,13.353296"
-	destination := "52.508309,13.355633"
-	avoid := "[areas]=bbox:13.37588,52.51061,13.34226,52.51892"
-
+	origin := "25.650711,-100.289449"
+	destination := "25.636026,-100.315968"
+	//avoid := "[areas]=bbox:13.37588,52.51061,13.34226,52.51892"
+	avoid := "[areas]=bbox:25.640095,-100.312389,25.654603,-100.289249"
 	apiUrl := "https://router.hereapi.com/v8/routes?" +
 		"origin=" + origin +
 		"&destination=" + destination +
@@ -154,14 +155,19 @@ func (mm *MapModel) CalcRoute() {
 		fmt.Println("Error parsing JSON response:", err)
 		return
 	}
-
 	var streetNames []string
+	dec, error := flexpolyline.Decode("BG45z9wB_-gp_FzF8GzKwMvMgPrEzFnVjcnG3I3S_YvM7Q7QvWvM7QvHnLrYnfjIrJzZriB7LrOjDrErEzF3DzFrE7GrEzF7G3IzKrO7G_J7Q7VzZjhB7QvW3mBj1BnGrJnLnQ7BrE7BnG3DzKzKvgBrT3_BnB_E7Qn4B_O_2B3DzK_E3InGrJjNrTjIzPrJvR7BjXnBjS7BrOjDnavC3SvH74BvC7LjD7L3D3IrEvH_JrJjIvMjD3IvCjN_EzevH3IjD_ET3DnBvCnGzKvRvgBnQzU7Q3S_EzFrJzKvCjDrT7VjIrJjSnVnVrYjS_TvCvCnVrd7G_JvRrY7BnGjDzF7Q3X_E7GnLzPjIjNnGzKnGrJ3IzP_JzP7BvC7GrEvHzKvH_JjDrEzK_O8V_iBoQ7agKrO4X7pBTnGUzFwC3N4D_EsEzFwHrJoGjIoG3IkI7LwHnLkIvMwH7LsiBr2BoV3hBkIvM0F3IsEvHsJrOgKrY8V7kBgFjI8GvW4D_JA_JnGnG_JrJjI3I7GjI_OvW7G_JnG3IrEnGrJrOvMvR_EjIrE7G3DvHjDvH4IvH8LnLwWvRA3DvC7G3DTvWwR7V8QvHgFrE4DzF0FvR8Q_JgKjIsJ3IoL_E8G7BwC_JgP_JgPjDsEvH0KjIkNrJsT7GgPrJoa3IwW3D0KjD4InG8QzFwMrEsJToBnGgK3DrE3NzF_Y_ErJT")
+	if error != nil {
+		fmt.Println("Error decoding polyline:", error)
+		return
+	}
+	fmt.Println(dec)
 
 	for _, route := range routeResponse.Routes {
 		for _, section := range route.Sections {
 			for _, span := range section.Spans {
 				for _, name := range span.Names {
-					fmt.Println("Street Name:", name.Value)
+					fmt.Println(name.Value)
 					streetNames = append(streetNames, name.Value)
 				}
 			}
